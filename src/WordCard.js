88290b368224c0aeb686fb9b4c5ccb541d31228d
WichonP <49986152+WichonP@users.noreply.github.com>
2019-09-22 02:47:44 +0700

    Add files via upload

--- src/WordCard.js
+++ src/WordCard.js
@@ -42,6 +42,7 @@ refreshPage(){
                 <div className="App">
                     <h1>Attempt : {this.state.attempt}</h1>
                         {Array.from(this.state.chars).map((c, i) => <CharacterCard value={c} key={i} attempt = {this.state.attempt} activationHandler={this.activationHandler}/>)}
+                    <p><img width = "300" height = "300" src = "https://scontent.fbkk8-3.fna.fbcdn.net/v/t1.0-9/51866008_1904750672984051_8109730310272319488_n.jpg?_nc_cat=111&_nc_oc=AQl8SRrtukl4RosZ1d1YkCvcYZC4sTO-PK86NdXFpTKRfPoERan7ku37M5oUH66u1Mw&_nc_ht=scontent.fbkk8-3.fna&oh=60f16d280c98d7d10af3cea194b87d4a&oe=5E3C0F8A"></img></p>
                     <p><h1>{this.state.completed? "Answer is : "+this.state.word: ""}</h1></p>
                     <p><h1>{this.state.completed? "You Corrrect!": ""}</h1></p>
                     <p><h1>{this.state.completed? <button onClick={this.refreshPage}>Play Again</button>: ""}</h1></p>

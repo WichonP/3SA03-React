25b3c611aa993df6d13b8a0e2992ba740fec4cb5
WichonP <49986152+WichonP@users.noreply.github.com>
2019-09-22 00:30:20 +0700

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React, { Component } from 'react';
+import WordCard from './WordCard';
+import './App.css';
+
+
+
+class App extends Component {
+    render() {
+        return ( 
+            <div className="App">
+               
+                  <WordCard value="hello"/>
+               
+            </div>
+        );
+    }
+}
+export default App;
---
+++ src/WordCard.js
@@ -0,0 +1,45 @@
+import React, { Component } from 'react';
+import CharacterCard from './CharacterCard';
+import './App.css';
+import _ from 'lodash';
+
+const prepareStateFromWord = (given_word) => {
+    let word = given_word.toUpperCase()
+    let chars = _.shuffle(Array.from(word))
+    return {
+        word,
+        chars,
+        attempt: 1,
+        guess: [],
+        completed: false
+    }
+}
+
+export default class Wordcard extends Component {
+    constructor(props){
+        super(props)
+        this.state = prepareStateFromWord(this.props.value)
+    }
+    activationHandler = (c) => {
+        console.log(`${c} has been activated.`)
+        let guess = [...this.state.guess, c]
+        this.setState({guess})
+        if(guess.length == this.state.chars.length){
+            if(guess.join('').toString() == this.state.word){
+                this.setState({guess: [], completed: true})
+            }else{    
+            let chars = _.shuffle(Array.from(this.state.word))
+            this.setState({guess: [], chars:chars, attempt: this.state.attempt + 1})
+            }
+        }
+}
+        render() {
+            return (
+                <div className="App">
+                    <p>Attempt : {this.state.attempt}</p>
+                        {Array.from(this.state.chars).map((c, i) => <CharacterCard value={c} key={i} attempt = {this.state.attempt} activationHandler={this.activationHandler}/>)}
+                    <p>{this.state.completed? this.state.word+"WIN": ""}</p>
+                </div>
+         );
+    }
+}
